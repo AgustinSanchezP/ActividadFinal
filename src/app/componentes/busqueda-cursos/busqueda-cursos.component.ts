c79675deb40f8283ede742933e2053368ae9f58f
Agustin <asanchezp15@gmail.com>
2022-02-16 15:22:37 +0100

    Lista de personajes

--- src/app/componentes/busqueda-cursos/busqueda-cursos.component.ts
+++ src/app/componentes/busqueda-cursos/busqueda-cursos.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Person } from '../../models/Person';
+import { PersonajesService } from '../../servicio/personajes.service';
 
 @Component({
   selector: 'app-busqueda-cursos',
   templateUrl: './busqueda-cursos.component.html',
   styleUrls: ['./busqueda-cursos.component.css']
 })
+
 export class BusquedaCursosComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  personajes: Person[] = [];
+
+
+  constructor(private route: ActivatedRoute, private personajesService: PersonajesService) {
+    this.personajes = personajesService.listadoPersonajes(); }
+
+
 
   ngOnInit(): void {
   }
---
+++ src/app/models/Person.ts
@@ -0,0 +1,8 @@
+export interface Person {
+  id: any,
+  name: string,
+  status: string,
+  species: string,
+  type: string,
+  gender?: string
+}
